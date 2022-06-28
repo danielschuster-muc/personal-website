8d99d8fa6f83aee2939a90acef0afd1b08ee5641
Daniel Schuster <daniel.a.schuster@t-online.de>
2022-06-28 21:20:36 +0200

    remove cname and add manifest.json for android chrome icons

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ const Document = () => {
           href="apple-touch-icon.png"
         />
         <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="manifest" href="manifest.json" />
       </Head>
       <body>
         <Main />
